d4af8f2c850075fe7873e0a02aeab2fc8d9191e0
Camilodez <hernandezz_camilo@javeriana.edu.co>
2024-02-04 17:43:52 -0500

    Modificacion del Codigo para que sea un poco mejor, create diccionario

--- menu.cpp
+++ menu.cpp
@@ -2,63 +2,18 @@
 #include <stdlib.h>
 #include <vector>
 #include <sstream>
+#include <fstream>
+#include <unordered_map>
 using namespace std;
 
 
-void menu();
-void menudeljuego();
-void opciones(int opcion);
-
-void ListadeAyudas();
+void Comandos(string auxilar,string argumento);
 
 int main(int argc, char *argv[]) {
 
-  int opcion = 0;
-  do{
-    menu();
-    cout<<"PS C:/Users/> $";
-    cin>> opcion;
-
-  opciones(opcion);
-
-  }while(opcion !=3);
-    return 0;
-}
-
-
-void menu(){
-  
-  cout<<"---------Scrabble---------"<< endl;
-  cout<<"1)Jugar"<< endl;
-  cout<<"2) Ayuda"<< endl;
-  cout<<"3) Salir" << endl;
-  cout<<"---------------------------"<< endl;
-}
-
-void opciones(int opcion){
-    switch (opcion)
-    {
-    case 1:
-      menudeljuego();
-      break;
-    
-    case 2:
-      ListadeAyudas();
-      break;
-      
-    case 3:
-
-      cout<<"Vuela Pronto!!"<<endl;
-      break;
-    
-    default:
-    cout<< "Opcion no Valida. Intentelo nuevamente" << endl;
-      break;
-    }
-}
-
- void menudeljuego() {
-    string comandos[10] = {"inicializar", "iniciar_inverso", "puntaje", "salir", "iniciar_arbol", "iniciar_arbol_inverso", "palabras_por_prefijo", "palabras_por_sufijo", "grafo_de_palabras", "posibles_palabras"};
+string comandos[10] = {"inicializar", "iniciar_inverso", "puntaje", "salir", "iniciar_arbol", 
+"iniciar_arbol_inverso", "palabras_por_prefijo", "palabras_por_sufijo", "grafo_de_palabras", 
+"posibles_palabras"};
     string input_usuario;
     
     cout << "Ingrese un comando: ";
@@ -72,18 +27,19 @@ void opciones(int opcion){
 
     bool existe = false;
 
+
     for (int i = 0; i < 10; i++) {
         if (comandos[i] == comando) {
             existe = true;
-
-            // Aquí puedes agregar el código correspondiente al comando encontrado
-
-            // Ahora verifica si hay argumentos adicionales y procesa según sea necesario
+            
+            string auxilar = comandos[i];
             string argumento;
+           
             while (isstream >> argumento) {
-                // Procesa los argumentos adicionales aquí
-                cout << "Argumento adicional: " << argumento << endl;
+                
             }
+
+             Comandos(auxilar, argumento);
 
             break; // Termina el bucle si encuentra el comando
         }
@@ -92,12 +48,39 @@ void opciones(int opcion){
     if (!existe) {
         cout << "Comando no reconocido. Inténtalo de nuevo." << endl;
     }
+
 }
-      
 
- 
+void Comandos(string auxilar,string argumento){
 
- void ListadeAyudas(){
-
-   cout<< "Ayuda" << endl;
- }
+    if(auxilar == "inicializar"){
+        
+    }else if (auxilar == "iniciar_inverso")
+    {
+      /* code */
+    }else if (auxilar == "puntaje")
+    {
+      /* code */
+    } else if (auxilar == "salir")
+    {
+      /* code */
+    } else if (auxilar == "iniciar_arbol")
+    {
+      /* code */
+    } else if (auxilar == "iniciar_arbol_inverso")
+    {
+      /* code */
+    } else if (auxilar == "palabras_por_prefijo")
+    {
+      /* code */
+    } else if (auxilar == "palabras_por_sufijo")
+    {
+      /* code */
+    } else if (auxilar == "grafo_de_palabras")
+    {
+      /* code */
+    } else if (auxilar == "posibles_palabras")
+    {
+      /* code */
+    }
+}
