6a03c9c0d25a4872a6883f913470cf415d86e3e9
aesteban28 <158786855+aesteban28@users.noreply.github.com>
2024-02-07 17:11:26 -0500

    Update Scrabble.h

--- Scrabble.h
+++ Scrabble.h
@@ -1,1 +1,19 @@
+#ifndef SCRABBLE_H
+#define SCRABBLE_H
+
+#include <string>
+#include <functional>
+
+void inicializarFuncion(const std::string& argumento);
+void iniciarInversoFuncion(const std::string& argumento);
+void puntajeFuncion(const std::string& argumento);
+void exitfuncion(const std::string& argumento);
+void iniciararbolFuncion(const std::string& argumento);
+void iniciararbolinversoFuncion(const std::string& argumento);
+void palabrasporprefijoFuncion(const std::string& argumento);
+void palabrasporsufijoFuncion(const std::string& argumento);
+void grafodepalabrasFuncion(const std::string& argumento);
+void posiblespalabrasFuncion(const std::string& argumento);
+
+#endif 
 
