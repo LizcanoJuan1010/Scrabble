2cc328ac7b10009bfefd6250d40f6b80074d9a43
aesteban28 <158786855+aesteban28@users.noreply.github.com>
2024-04-17 19:19:34 -0500

    Update Trie.h

--- Trie.h
+++ Trie.h
@@ -3,6 +3,8 @@
 
 #include "TrieNode.h"
 #include <string>
+#include <vector>
+#include "Dictionary.h"
 
 class Trie {
 private:
@@ -15,6 +17,8 @@ public:
 
     TrieNode *search(std::string word);
     void PrintLexical(TrieNode *current, std::string prefix, std::string suffix);
+void recolectarPalabras(TrieNode* start, const std::string& prefix, std::vector<std::string>& palabras, const Dictionary& dictionary) const;
+ void recolectarPalabrasInversas(TrieNode* node, const std::string& acumulado, std::vector<std::string>& palabras) const;
 };
 
 
@@ -22,3 +26,5 @@ public:
 
 
 #endif // TRIE_H
+
+
